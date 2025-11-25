65e5a413b6a59299f76f4d4a8f58f5cd0f25dea8
Lutfi Ismail <lutfiismail52@gmail.com>
2025-11-25 17:00:09 +0700

    update style.css and modified script.js logic

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-const textInput = document.getElementById("text-input");
+const userInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
-checkBtn.addEventListener("click", () => {
-  const textInputCleaned = textInput.value
+// Function for checking user input is palindrome or not
+const checkPalindrome = (input) => {
+  // Cleaning non-alphanumeric user input
+  const userInputCleaned = userInput.value
     .trim()
     .replace(/[^a-z0-9]/gi, "")
     .toLowerCase();
 
-  if (textInputCleaned === "") {
+  // Checking user input is empty or not
+  if (userInputCleaned === "") {
     alert("Please input a value");
     result.innerText = "";
     return;
   }
 
-  const textInputReversed = textInputCleaned.split("").reverse().join("");
-  if (textInputCleaned === textInputReversed) {
-    result.innerText = `${textInputCleaned} is a palindrome`;
+  // Checking user input can spelling the same way both forward and backward or not
+  const userInputReversed = userInputCleaned.split("").reverse().join("");
+  if (userInputCleaned === userInputReversed) {
+    result.innerText = `${userInput.value} is a palindrome`;
   } else {
-    result.innerText = `${textInputCleaned} is not a palindrome`;
+    result.innerText = `${userInput.value} is not a palindrome`;
+  }
+};
+
+// Call check palindrome function when user press Enter button
+userInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkPalindrome();
   }
 });
+
+// Call check palindrome function when user click Check button
+checkBtn.addEventListener("click", checkPalindrome);
